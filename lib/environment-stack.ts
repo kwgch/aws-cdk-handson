8e78b8ee51b71d3742d70582e82496a0f4eadcc9
m.kwgch <m.kwgch@gmail.com>
2024-09-16 07:14:40 +0000

    website hosting

--- lib/environment-stack.ts
+++ lib/environment-stack.ts
@@ -1,30 +1,43 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment'; //追加モジュール
-import * as path from 'path' //追加モジュール
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path'
+import { PolicyStatement, Effect, ArnPrincipal } from 'aws-cdk-lib/aws-iam';
 
-// 今日の日付を取得する(yyyyMMdd形式)
 const today = new Date;
 const date = `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}`;
-
-// 自分の名前を設定する(小文字)
 const name = 'kwgch';
 
 export class EnvironmentStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // S3の設定
-    const bucket = new s3.Bucket(this, "Step1Bucket", {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    const bucket = new s3.Bucket(this, 'Step1Bucket', {
+      // パブリックアクセスを許可する
+      publicReadAccess: true,
+      // 静的ウェブホスティングを有効化する
+      websiteIndexDocument: 'index.html',
+      // ACLを通じたアクセスをブロックしつつ、バケットポリシーを通じたアクセスを許可する
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
       bucketName: `${date}-${name}-step1`,
     });
 
-    // 以下追加。ローカルファイルを上記のS3へアップロードする。
     const deployment = new s3deploy.BucketDeployment(this, 'UploadStaticContents', {
-      sources: [s3deploy.Source.asset(path.join('./contents.zip'))], // npmコマンド実行階層から見てのパスを指定
-      destinationBucket: bucket, // 上記のS3を指定
+      sources: [s3deploy.Source.asset(path.join('./contents.zip'))], 
+      destinationBucket: bucket, 
     });
+
+    // 読み取り専用のバケットポリシーを作成する
+    const allowPublicAccessBucketPolicy = new PolicyStatement({
+      sid: 'Allow Public Access',
+      effect: Effect.ALLOW,
+      actions: ['s3:GetObject'],
+      principals: [new ArnPrincipal('*')],  //難しめ
+      resources: [bucket.bucketArn + '/*'], //ちょっと難しめ
+    });
+
+    // バケットポリシーをS3バケットに適用
+    bucket.addToResourcePolicy(allowPublicAccessBucketPolicy);
   }
 }
