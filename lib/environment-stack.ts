3b7c2d23513704df45460d3bbca83f8bac7adffe
m.kwgch <m.kwgch@gmail.com>
2024-09-16 06:39:38 +0000

    create s3 bucket

--- lib/environment-stack.ts
+++ lib/environment-stack.ts
@@ -1,19 +1,12 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class EnvironmentStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'EnvironmentQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'EnvironmentTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // S3の設定
+    const bucket = new s3.Bucket(this, "MyFirstBucket");
   }
 }
