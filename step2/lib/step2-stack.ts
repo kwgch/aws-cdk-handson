719780020f2f8b3dac0f91e6bbfb4106d157d95e
m.kwgch <m.kwgch@gmail.com>
2024-09-17 12:23:08 +0000

    add API Gateway

--- step2/lib/step2-stack.ts
+++ step2/lib/step2-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class Step2Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -25,5 +26,27 @@ export class Step2Stack extends cdk.Stack {
     });
 
     table.grantWriteData(lambdaFunction);
+
+    // API GatewayのREST API作成
+    const api = new apigateway.RestApi(this, 'ApplicantApi', {
+      restApiName: 'Applicant Service',
+      description: 'This service serves applicant data.',
+      deployOptions: {
+        stageName: 'dev',  // ステージ名を dev に設定
+      }
+    });
+
+    // 'applicant' リソースを作成
+    const applicant = api.root.addResource('applicant');
+
+    // POST メソッドを追加し、Lambda 関数と統合
+    const postIntegration = new apigateway.LambdaIntegration(lambdaFunction);
+    applicant.addMethod('POST', postIntegration, {
+      operationName: 'CreateApplicant',
+      methodResponses: [
+        { statusCode: '200' },  // 成功時のレスポンス定義
+        { statusCode: '400' },  // バリデーションエラー等
+      ]
+    });
   }
 }
